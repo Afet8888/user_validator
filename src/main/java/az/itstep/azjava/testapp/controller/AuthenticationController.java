b30c87509eb117ef4596695fdaed67ee4fa29408
Rustam Sultansoy <sultansoy@itstep.local>
2018-04-14 17:48:05 +0400

    token from header

--- src/main/java/az/itstep/azjava/testapp/controller/AuthenticationController.java
+++ src/main/java/az/itstep/azjava/testapp/controller/AuthenticationController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 import java.util.Objects;
@@ -38,8 +35,11 @@ public class AuthenticationController {
     }
 
     @PutMapping("/hello")
-    public ResponseEntity<String> sayHello(@RequestBody Key key) {
-        User user = authenticatedUsers.get(key.getValue());
+    public ResponseEntity<String> sayHello(@RequestHeader HttpHeaders httpHeaders) {
+        //get key from header
+
+        String key = httpHeaders.get("token").get(0);
+        User user = authenticatedUsers.get(key);
         if(Objects.nonNull(user)) {
             String res = "Hello, " + user.getUsername();
             return ResponseEntity
