e2907155920feda5b816ccd6c3e3b029c390730a
Rustam Sultansoy <sultansoy@itstep.local>
2018-04-14 17:29:19 +0400

    add response entities

--- src/main/java/az/itstep/azjava/testapp/controller/AuthenticationController.java
+++ src/main/java/az/itstep/azjava/testapp/controller/AuthenticationController.java
@@ -4,6 +4,9 @@ import az.itstep.azjava.testapp.model.Key;
 import az.itstep.azjava.testapp.model.User;
 import az.itstep.azjava.testapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,24 +26,29 @@ public class AuthenticationController {
     private UserService userService;
 
     @PostMapping("/signin")
-    public String signIn(@RequestBody User user) {
+    public ResponseEntity<?> signIn(@RequestBody User user) {
         User search = userService.getUser(user.getUsername());
         if(Objects.nonNull(search)
                 && Objects.equals(search.getPassword(), user.getPassword()))  {
             String key = UUID.randomUUID().toString();
             authenticatedUsers.put(key, search);
-            return key;
+            return ResponseEntity.ok(key);//200 OK
         }
-        return "";
+        return ResponseEntity.noContent().build();//HTTP STATUS 204
     }
 
     @PutMapping("/hello")
-    public String sayHello(@RequestBody Key key) {
+    public ResponseEntity<String> sayHello(@RequestBody Key key) {
         User user = authenticatedUsers.get(key.getValue());
         if(Objects.nonNull(user)) {
-            return "Hello, " + user.getUsername();
+            String res = "Hello, " + user.getUsername();
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(res);//200 OK other style
         }
-        return "ERROR";
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .build();//HTTP STATUS 401
     }
 
 }
