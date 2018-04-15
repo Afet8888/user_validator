9d246c84e7f9055ff9ac3c14be4684be7e553645
Rustam Sultansoy <sultansoy@itstep.local>
2018-04-15 16:13:21 +0400

    add data jpa

--- src/main/java/az/itstep/azjava/testapp/model/Human.java
+++ src/main/java/az/itstep/azjava/testapp/model/Human.java
@@ -4,16 +4,24 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import javax.persistence.*;
 import java.util.Date;
 
 @Data
 @ToString
+@Entity
+@Table(name = "people")
 public class Human {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String name;
     private String surname;
     private String gender;
-    private Date dateOfBirth;
+    @Column(name = "my_custom_date_field_name")
+    private Date dateOfBirth;//date_of_birth
     private Human mother;
     private Human father;
 }
