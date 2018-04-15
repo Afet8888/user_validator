316b07809fe25b3b6cb55b8310fbeedc57e4a44f
Rustam Sultansoy <sultansoy@itstep.local>
2018-04-15 16:22:02 +0400

    remove CrudService

--- src/main/java/az/itstep/azjava/testapp/service/HumanService.java
+++ src/main/java/az/itstep/azjava/testapp/service/HumanService.java
@@ -2,6 +2,6 @@ package az.itstep.azjava.testapp.service;
 
 import az.itstep.azjava.testapp.model.Human;
 
-public interface HumanService extends CrudService<Human, Integer> {
+public interface HumanService {
 
 }
