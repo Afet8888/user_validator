648214c151af8806cccfcc19ac95cb50a90b5468
Rustam Sultansoy <sultansoy@itstep.local>
2018-04-28 15:37:59 +0400

    CORS Link

--- src/main/java/az/itstep/azjava/testapp/Application.java
+++ src/main/java/az/itstep/azjava/testapp/Application.java
@@ -22,8 +22,6 @@ public class Application {
                     List<File> from directory with ID = {id}
 
     List<File> getFilesFromDirectory(Integer directoryId)
-
-
      */
     //https://spring.io/blog/2015/06/08/cors-support-in-spring-framework
     public static void main(String[] args) {
